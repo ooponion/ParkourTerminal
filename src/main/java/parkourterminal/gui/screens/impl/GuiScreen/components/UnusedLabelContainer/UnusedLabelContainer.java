d2c360200360b4511538561203a3b0dc72ffb9e2
Northagai <northagai@gmail.com>
2025-04-01 20:01:54 +0800

    <Fix> 修复UnusedContainer的不滑动bug和宽度过小问题

--- src/main/java/parkourterminal/gui/screens/impl/GuiScreen/components/UnusedLabelContainer/UnusedLabelContainer.java
+++ src/main/java/parkourterminal/gui/screens/impl/GuiScreen/components/UnusedLabelContainer/UnusedLabelContainer.java
@@ -25,9 +25,8 @@ public class UnusedLabelContainer extends Container {
         this.setEnabled(false);
         this.setPadding(new Padding(7));
         this.setLayoutManager(new noWarpLinearLayout(LayoutDirection.VERTICAL,2));
-        this.setWidth(40);
+        this.setWidth(100);
         scrollBar.setColor(0x00000000,0x40000000);
-
     }
 
     @Override
@@ -36,14 +35,16 @@ public class UnusedLabelContainer extends Container {
         if(!isEnabled()){
             return;
         }
+        scrollBar.draw(mouseX,mouseY,partialTicks);
+        scrollBar.Update();
         this.setY((int) (0-scrollBar.getInterpolatingContentOffset()));
         GuiScreen guiScreen=Minecraft.getMinecraft().currentScreen;
         if(guiScreen==null){
             return;
         }
         ScissorHelper.EnableScissor(guiScreen.width-getWidth(),0,getWidth(), guiScreen.height);
-        ShapeDrawer.drawRoundedRect(getX(),getY(),getWidth(),getHeight(),0xAA312e72,3);
-        ShapeDrawer.drawRoundedRectBorder(getX(),getY(),getWidth(),getHeight(),0xFF5d58c8,3);
+        ShapeDrawer.drawRoundedRect(getX(),0,getWidth(),getHeight(),0xAA312e72,3);
+        ShapeDrawer.drawRoundedRectBorder(getX(),0,getWidth(),getHeight(),0xFF5d58c8,3);
         super.draw(mouseX, mouseY, partialTicks);
         ScissorHelper.DisableScissor();
 
@@ -56,7 +57,8 @@ public class UnusedLabelContainer extends Container {
         }
         this.setX(guiScreen.width-getWidth());
         this.setHeight(guiScreen.height);
-        scrollBar.setHeight(this.getComponentsTotalHeight());
+        scrollBar.setPosition(guiScreen.width-scrollBar.getWidth(),0);
+        scrollBar.setHeight(guiScreen.height);
         disableTip.setEnabled(false);
         expansionFrame.Update();
     };
@@ -95,5 +97,15 @@ public class UnusedLabelContainer extends Container {
     @Override
     public boolean isMouseOver(int mouseX, int mouseY) {
         return isEnabled()&&super.isMouseOver(mouseX, mouseY);
+    }
+    @Override
+    public void deleteComponent(UIComponent component){
+        super.deleteComponent(component);
+        scrollBar.UpdateContentSize(this.getComponentsTotalHeight()+getPadding().top+getPadding().bottom);
+    }
+    @Override
+    public void addComponent(UIComponent component) {
+        super.addComponent(component);
+        scrollBar.UpdateContentSize(this.getComponentsTotalHeight()+getPadding().top+getPadding().bottom);
     }
 }
